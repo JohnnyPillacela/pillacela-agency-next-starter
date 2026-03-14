693a3111a95538f866b53cdac91ddf0552b90d6b
Johnny Pillacela <johnnybelesaca@gmail.com>
2026-03-14 01:26:41 -0400

    Add BriefServices and Services components for single and multi-page website offerings

---
+++ components/briefs/BriefServices.tsx
@@ -0,0 +1,21 @@
+type Props = {
+    dict: any
+}
+// This is for people who only pay for a singel webpage. 
+export default function BriefServices({ dict }: Props) {
+    return (
+        <section id="services" className="py-24">
+            <div className="container mx-auto">
+                <h2 className="text-3xl font-bold mb-8">
+                    {dict.title}
+                </h2>
+
+                <ul className="grid gap-4 md:grid-cols-2">
+                    {dict.items.map((item: string, index: number) => (
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
+            </div>
+        </section>
+    )
+}
