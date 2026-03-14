2eb78f72a2ddb7a74b5ef3506955ca59a3a644a2
Johnny Pillacela <johnnybelesaca@gmail.com>
2026-03-14 00:06:21 -0400

    Add HomePage component for marketing template

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <main>
+            <h1>Pillacela Agency Starter</h1>
+            <p>Your reusable marketing website template.</p>
+        </main>
+    );
+}
