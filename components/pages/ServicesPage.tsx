31aebe509d999c9f6f069b59490a7a7f19d43f93
Johnny Pillacela <johnnybelesaca@gmail.com>
2026-03-14 02:09:19 -0400

    Main ServicesPage

--- components/pages/ServicesPage.tsx
+++ components/pages/ServicesPage.tsx
@@ -1,5 +1,7 @@
 // /components/pages/ServicesPage.tsx
 
+import ServicesList from "../sections/services/services-list"
+
 type Props = {
     dict: any
 }
@@ -10,6 +12,7 @@ export default function ServicesPage({ dict }: Props) {
             <h1>{dict.title2}</h1>
             <p>{dict.subtitle2}</p>
             <p>{dict.cta}</p>
+            <ServicesList dict={dict} />
         </>
     )
 }
