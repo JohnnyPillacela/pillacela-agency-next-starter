5031656120eca8d2a67bd60c0c16f9c52f847905
Johnny Pillacela <johnnybelesaca@gmail.com>
2026-03-17 02:08:55 -0400

    Add Spanish services page component

---
+++ app/es/(marketing)/services/page.tsx
@@ -0,0 +1,9 @@
+import ServicesPage from "@/components/pages/ServicesPage"
+
+export default function Page() {
+    return (
+        <>
+            <ServicesPage locale="es" />
+        </>
+    )
+}
